5567e46eab669d1bdf9045e680cb1a4d43f9fbc3
MaxHarper <islowget@gmail.com>
2022-05-01 16:51:18 +0300

    Module 2

--- module2/Program.cs
+++ module2/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace module2
+﻿using System;
+
+namespace module2
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            string FullName = "Иванов Иван Ивановчич";
+            byte Age = 33;
+            string Email = "mail@mail.com";
+            byte ScroreProgrammer = 15;
+            byte ScroreMath = 10;
+            byte ScrorePhysix = 8;
+
+            Console.WriteLine($"Ф.И.О. - {FullName} \n" +
+                              $"Возраст {Age} \n" +
+                              $"Email {Email} \n " +
+                              $"Баллы по программированию - {ScroreProgrammer} \n" +
+                              $"Баллы по математике - {ScroreMath} \n" +
+                              $"Баллы по физике - {ScrorePhysix}");
+            Console.ReadKey();
+
+            int ScoreSumm = ScroreMath + ScrorePhysix + ScroreProgrammer;
+            Console.WriteLine($"Сумма балов - {ScoreSumm} \nСрединй бал - {ScoreSumm / 3}");
+            Console.ReadKey();
         }
     }
 }
